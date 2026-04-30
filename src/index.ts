bcfc5f0a9c2e573e4a1f8a002c7a2e2694851ce0
Djeison Santos <djeison.adventista@gmail.com>
2026-04-30 12:25:14 -0100

    B

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+
 import "dotenv/config";
 import express, { type Request, type Response } from "express";
+import cors from "cors";
 import { router as serviceRouter } from "./routes/servico.route.js";
 import { router as usersRouter } from "./routes/users.route.js";
 import { router as orcamentoRouter } from "./routes/orcamento.route.js";
@@ -8,13 +10,19 @@ import { router as prestadorRouter } from "./routes/prestador.route.js";
 import { router as prestacaoServicoRouter } from "./routes/prestacao-servico.route.js";
 import { router as empresaRouter } from "./routes/empresa.route.js";
 import { router as categoriaRouter } from "./routes/categoria.route.js";
-import { swaggerSpec} from "./docs/swagger.js"
+import { swaggerSpec } from "./docs/swagger.js"
 import swaggerUi from "swagger-ui-express"
 import { ApolloServer } from "@apollo/server";
 import { resolvers, typeDefs } from "./graphql/index.js";
 import { expressMiddleware } from "@as-integrations/express5";
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/service", serviceRouter)
@@ -27,17 +35,17 @@ app.use("/empresa", empresaRouter)
 app.use("/categoria", categoriaRouter)
 
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec) )
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
-const graphqlServer = new ApolloServer({  
+const graphqlServer = new ApolloServer({
     typeDefs,
     resolvers,
-}) 
+})
 
 await graphqlServer.start();
 
 app.use("/graphql", expressMiddleware(graphqlServer, {
-    context: async ({ req }) => ({ 
+    context: async ({ req }) => ({
         token: req.headers.authorization,
         DB_HOST: process.env.DB_HOST,
         DB_USER: process.env.DB_USER,
